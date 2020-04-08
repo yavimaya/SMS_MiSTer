6bb2af59a7ae8638117c818486b88f8edd761441
José Manuel Barroso Galindo <theypsilon@gmail.com>
2020-04-09 01:37:07 +0200

    BOT: Change Id '37 for Id '0f (patch 1).

--- sys/hps_io.v
+++ sys/hps_io.v
@@ -472,7 +472,7 @@ always@(posedge clk_sys) begin
 							end
 							
 					// Reading user_io raw joy
-					'h37: io_dout <= joy_raw;
+					'h0f: io_dout <= joy_raw;
 
 					//menu mask
 					'h2E: if(byte_cnt == 1) io_dout <= status_menumask;
