96d2f779add8d8afd567e02368416118eae3039a
Miguel-T80c <the80scomputers@gmail.com>
2020-04-21 17:45:00 +0200

    Added Joystick DB15 Support

--- SMS.sv
+++ SMS.sv
@@ -118,7 +118,7 @@ module emu
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
 	output	USER_OSD,
-	output	USER_MODE,
+	output  [1:0] USER_MODE,
 	input	[7:0] USER_IN,
 	output	[7:0] USER_OUT,
 
@@ -137,11 +137,14 @@ localparam SP64     = 1'b0;
 
 assign ADC_BUS  = 'Z;
 
-wire   joy_split, joy_mdsel;
-wire   [5:0] joy_in = {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]};
-assign USER_OUT  = |status[31:30] ? {3'b111,joy_split,3'b111,joy_mdsel} : '1;
-assign USER_MODE = |status[31:30] ;
-assign USER_OSD  = joydb9md_1[7] & joydb9md_1[5]; // A�adir esto para OSD
+wire         CLK_JOY = CLK_50M;         //Assign clock between 40-50Mhz
+wire   [2:0] JOY_FLAG  = {status[30],status[31],status[29]}; //Assign 3 bits of status (31:29) o (63:61)
+wire         JOY_CLK, JOY_LOAD, JOY_SPLIT, JOY_MDSEL;
+wire   [5:0] JOY_MDIN  = JOY_FLAG[2] ? {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]} : '1;
+wire         JOY_DATA  = JOY_FLAG[1] ? USER_IN[5] : '1;
+assign       USER_OUT  = JOY_FLAG[2] ? {3'b111,JOY_SPLIT,3'b111,JOY_MDSEL} : JOY_FLAG[1] ? {6'b111111,JOY_CLK,JOY_LOAD} : '1;
+assign       USER_MODE = JOY_FLAG[2:1] ;
+assign       USER_OSD  = joydb_1[10] & joydb_1[6]; // A�adir esto para OSD
 
 assign VGA_F1 = 0;
 
@@ -168,7 +171,8 @@ parameter CONF_STR = {
 	"C,Cheats;",
 	"H1OO,Cheats enabled,ON,OFF;",
 	"-;",
-	"OUV,Serial SNAC DB9MD,Off,1 Player,2 Players;",
+	"OUV,UserIO Joystick,Off,DB9MD,DB15 ;",
+	"OT,UserIO Players, 1 Player,2 Players;",
 	"-;",
 	"D0R6,Load Backup RAM;",
 	"D0R7,Save Backup RAM;",
@@ -239,39 +243,39 @@ wire [63:0] img_size;
 wire        forced_scandoubler;
 wire [21:0] gamma_bus;
 
-wire [6:0] joy_0 = |status[31:30] ? {
-	joydb9md_1[7], // Pause		-> 6 * Start
-	joydb9md_1[4], // btn_fire2	-> 5 * B
-	joydb9md_1[6], // btn_fire1	-> 4 * A
-	joydb9md_1[3], // btn_up	-> 3 * U
-	joydb9md_1[2], // btn_down	-> 2 * D
-	joydb9md_1[1], // btn_left	-> 1 * L
-	joydb9md_1[0], // btn_right	-> 0 * R 
-	} 
-	: joy_0_USB;
+//S B A U D L R 
+wire [31:0] joy_0 = joydb_1ena ? (OSD_STATUS? 32'b000000 : {joydb_1[10],joydb_1[5:0]}) : joy_0_USB;
+wire [31:0] joy_1 = joydb_2ena ? (OSD_STATUS? 32'b000000 : {joydb_2[10],joydb_2[5:0]}) : joydb_1ena ? joy_0_USB : joy_1_USB;
 
-wire [6:0] joy_1 =  status[31]    ? {
-	joydb9md_2[7], // Pause		-> 6 * Start
-	joydb9md_2[4], // btn_fire2	-> 5 * B
-	joydb9md_2[6], // btn_fire1	-> 4 * A
-	joydb9md_2[3], // btn_up	-> 3 * U
-	joydb9md_2[2], // btn_down	-> 2 * D
-	joydb9md_2[1], // btn_left	-> 1 * L
-	joydb9md_2[0], // btn_right	-> 0 * R 
-	} 
-	: status[30] ? joy_0_USB : joy_1_USB;
+wire [15:0] joydb_1 = JOY_FLAG[2] ? JOYDB9MD_1 : JOY_FLAG[1] ? JOYDB15_1 : '0;
+wire [15:0] joydb_2 = JOY_FLAG[2] ? JOYDB9MD_2 : JOY_FLAG[1] ? JOYDB15_2 : '0;
+wire        joydb_1ena = |JOY_FLAG[2:1]              ;
+wire        joydb_2ena = |JOY_FLAG[2:1] & JOY_FLAG[0];
 
-
-
-reg [15:0] joydb9md_1,joydb9md_2;
+//----BA 9876543210
+//----MS ZYXCBAUDLR
+reg [15:0] JOYDB9MD_1,JOYDB9MD_2;
 joy_db9md joy_db9md
 (
-  .clk       ( clk_sys    ), //35-50MHz
-  .joy_split ( joy_split  ),
-  .joy_mdsel ( joy_mdsel  ),
-  .joy_in    ( joy_in     ),
-  .joystick1 ( joydb9md_1 ),
-  .joystick2 ( joydb9md_2 )	  
+  .clk       ( CLK_JOY    ), //40-50MHz
+  .joy_split ( JOY_SPLIT  ),
+  .joy_mdsel ( JOY_MDSEL  ),
+  .joy_in    ( JOY_MDIN   ),
+  .joystick1 ( JOYDB9MD_1 ),
+  .joystick2 ( JOYDB9MD_2 )	  
+);
+
+//----BA 9876543210
+//----LS FEDCBAUDLR
+reg [15:0] JOYDB15_1,JOYDB15_2;
+joy_db15 joy_db15
+(
+  .clk       ( CLK_JOY   ), //48MHz
+  .JOY_CLK   ( JOY_CLK   ),
+  .JOY_DATA  ( JOY_DATA  ),
+  .JOY_LOAD  ( JOY_LOAD  ),
+  .joystick1 ( JOYDB15_1 ),
+  .joystick2 ( JOYDB15_2 )	  
 );
 
 
@@ -286,7 +290,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3), .WIDE(0)) hps_io
 	.joystick_1(joy_1_USB),
 	.joystick_2(joy[2]),
 	.joystick_3(joy[3]),
-	.joy_raw({joydb9md_1[4],joydb9md_1[6],joydb9md_1[3:0]}), //Menu Dirs, A:Action B:Back (OSD)
+	.joy_raw(OSD_STATUS? (joydb_1[5:0]|joydb_2[5:0]) : 6'b000000 ), //Menu Dirs, A:Action B:Back (OSD)
 
 	.buttons(buttons),
 	.status(status),
--- sys/hps_io.v
+++ sys/hps_io.v
@@ -473,6 +473,8 @@ always@(posedge clk_sys) begin
 							
 					// Reading user_io raw joy
 					'h0f: io_dout <= joy_raw;
+// buttons and switches
+'h01: cfg <= io_din;
 
 					//menu mask
 					'h2E: if(byte_cnt == 1) io_dout <= status_menumask;
